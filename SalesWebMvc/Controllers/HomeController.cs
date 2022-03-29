6e879071ca940a6a5377410cd7a5c0799a1cf3e3
RaulORios <raul.orios89@outlook.com>
2022-03-29 16:50:12 -0300

    First controller and Razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "First controller and Razor pages test";
 
             return View();
         }
